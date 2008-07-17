fa4d8078f663755b7151ef65a9c2b719b5bd172c
GameFreak7744 <GameFreak7744@7f70f224-fa50-0410-840f-91b469c855e5>
2008-07-17 22:24:15 +0000

    Adding MIT license to source files; main.c, data_struct.h, packet_struct.h

--- Test Packet/main.c
+++ Test Packet/main.c
@@ -1,3 +1,31 @@
+/*
+ *  main.c
+ *  Packet
+ *
+ *  Created by Joey Sabey on 16/07/08.
+ *  Copyright (c) 2008 Joey Sabey, et al.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -118,7 +146,7 @@ void packet_free(struct packet *pack)
 		struct data **z = y;
 		y = &(*y)->next;
 		//free((*z)->name);
-		//printf((*y)->name);
+		printf((*y)->name);
 		//printf(pack->entry->name);
 		//free((*z)->value);
 		//free(*z);
